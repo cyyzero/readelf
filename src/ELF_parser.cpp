cbd0efa4488550fa2e0a49482a06ea78466b85c1
cyyzero <cyyzero16@qq.com>
2018-09-05 00:45:37 +0800

    add copy constructor and copy asignment operation

--- src/ELF_parser.cpp
+++ src/ELF_parser.cpp
@@ -21,47 +21,71 @@ namespace elf_parser
 ELF_parser::ELF_parser()
     : m_fd(-1), m_program_length(0) , m_mmap_program(nullptr) { }
 
-ELF_parser::ELF_parser(const std::string &file_path)
+ELF_parser::ELF_parser(const std::string& file_path)
+    : m_file_path(file_path)
 {
-    load_memory_map(file_path);
+    load_memory_map();
+}
+
+ELF_parser::ELF_parser(std::stirng&& file_path)
+    : m_file_path(std::move(file_path))
+{
+    load_memory_map();
+}
+
+ELF_parser::ELF_parser(const ELF_parser& object)
+    : m_file_path(object.m_file_path)
+{
+    load_memory_map();
 }
 
 ELF_parser::ELF_parser(ELF_parser&& object)
-    : m_fd(object.m_fd), m_program_length(object.m_program_length), m_mmap_program(object.m_mmap_program)
+    : m_file_path(std::move(object.m_file_path)), m_fd(object.m_fd),
+    m_program_length(object.m_program_length), m_mmap_program(object.m_mmap_program)
 {
     object.initialize_members();
 }
 
+ELF_parser& ELF_parser::operator=(const ELF_parser& object)
+{
+    initialize_members(object.m_file_path, object.m_fd, object.m_program_length,
+                       object.m_mmap_program);
+}
+
 ELF_parser& ELF_parser::operator=(ELF_parser&& object)
 {
-    initialize_members(object.m_fd, object.m_program_length, object.m_mmap_program);
+    initialize_members(std::move(object.m_file_path), object.m_fd,
+                       object.m_program_length, object.m_mmap_program);
 
     object.initialize_members();
 }
 
 ELF_parser::~ELF_parser()
 {
-    if (m_fd != -1)
-    {
-        close_memory_map();
-    }
+
+    close_memory_map();
 }
 
 void ELF_parser::load_file(const std::string& path_name)
 {
-    if (m_fd != -1)
-    {
-        close_memory_map();
-    }
-    load_memory_map(path_name);
+    m_file_path = path_name;
+    close_memory_map();
+    load_memory_map();
 }
 
-void ELF_parser::load_memory_map(const std::string& path_name) const
+void ELF_parser::load_file(std::string&& path_name)
+{
+    m_file_path = std::move(file_path);
+    close_memory_map();
+    load_memory_map();
+}
+
+void ELF_parser::load_memory_map()
 {
     void *mmap_res;
     struct stat st;
 
-    if ((m_fd = open(path_name.c_str(), O_RDONLY)) == -1)
+    if ((m_fd = open(m_path_name.c_str(), O_RDONLY)) == -1)
     {
         ERROR_EXIT("open");
     }
@@ -82,8 +106,13 @@ void ELF_parser::load_memory_map(const std::string& path_name) const
     m_mmap_program = static_cast<std::uint8_t *>(mmap_res);
 }
 
-void ELF_parser::close_memory_map() const
+void ELF_parser::close_memory_map()
 {
+    if (m_fd == -1)
+    {
+        return;
+    }
+
     if (munmap(static_cast<void *>(m_mmap_program), m_program_length) == -1)
     {
         ERROR_EXIT("munmap");
@@ -97,11 +126,13 @@ void ELF_parser::close_memory_map() const
     initialize_members();
 }
 
-void ELF_parser::initialize_members(int fd, std::size_t program_length, std::uint8_t *mmap_program)
+void ELF_parser::initialize_members(std::string file_path, int fd, 
+                                    std::size_t program_length, std::uint8_t *mmap_program)
 {
+    m_file_path = std::move(file_path);
     m_fd = fd;
     m_program_length = program_length;
-    close_memory_map = mmap_program;
+    m_mmap_program = mmap_program;
 }
 
 } // namespace elf_parser
--- src/ELF_parser.h
+++ src/ELF_parser.h
@@ -13,21 +13,24 @@ class ELF_parser
 public:
     ELF_parser();
     ELF_parser(const std::string& file_path);
-    ELF_parser(const ELF_parser&) = delete;
+    ELF_parser(std::string&& file_path);
+    ELF_parser(const ELF_parser& object);
     ELF_parser(ELF_parser&& object);
-
-    ELF_parser& operator=(const ELF_parser&) = delete;
+    ELF_parser& operator=(const ELF_parser& object);
     ELF_parser& operator=(ELF_parser&& object);
     ~ELF_parser();
 
     void load_file(const std::string& file_path);
+    void load_file(std::string& file_path);
 
 private:
     void load_memory_map(const std::string& file_path);
+    void initialize_members(std::string file_path = std::string(),
+                            int fd = -1,
+                            std::size_t program_length = 0,
+                            std::uint8_t *mmap_program = nullptr);
 
-    void initialize_members(int fd = -1, std::size_t program_length = 0, std::uint8_t *mmap_program = nullptr);
-    
-    //std::string m_file_name;
+    std::string m_file_path;
     int m_fd;
     std::size_t m_program_length;
     std::uint8_t *m_mmap_program;
